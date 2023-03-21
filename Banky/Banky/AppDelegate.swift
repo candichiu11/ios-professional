ea5f6a6883844abdd59d604b75b16654febcd00b
Candi Chiu <li-lun.chiu@kfzteile24.de>
2023-03-21 15:24:39 +0100

    Add login screen

---
+++ Banky/Banky/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Banky
+//
+//  Created by Candi Chiu on 17.03.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+ 
+
+
+}
+
